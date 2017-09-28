464e76656cb429ac0e89494a8fb0a6b26bdcedf1
erdnando <erdnando@gmail.com>
2017-09-28 08:10:30 -0700

    subiendo paquete final

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,14 @@
+package clinic.programming.training;
+
+public class Application {
+    
+    public Application() {
+        System.out.println ("Inside Application Erdnando..................");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application, Erdnando..................");
+	Application app = new Application();
+    }
+}
